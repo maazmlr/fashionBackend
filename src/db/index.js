7d1143f71200d094e5682908131fdf258a870a5f
muhammad maaz <compiletimeterror@gmail.com>
2024-06-11 23:54:03 +0500

    dnn

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constant.js";
 const connection = async () => {
   try {
     const connection = await mongoose.connect(
-      `mongodb+srv://itxelegantmansoori:basit1234@cluster0.wlbop4e.mongodb.net/fyp?retryWrites=true&w=majority `
+      `mongodb+srv://hashiradnan55:duV57gxalfflwe21@quizgame-mernstack.8jnxvsr.mongodb.net/?retryWrites=true&w=majority `
     );
     console.log(connection.connection.host, "mogodb connedted");
   } catch (error) {

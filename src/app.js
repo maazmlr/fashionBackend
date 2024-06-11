8ada494a4faece0bb8b92baaa81cdc0aa5e64dc4
muhammad maaz <compiletimeterror@gmail.com>
2024-06-11 23:24:29 +0500

    sns

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10kb" }));
+// app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
